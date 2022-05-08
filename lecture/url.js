250b04ce945d40360ed220c3d9114a33bc5efa0f
조형우 <guddndhk@naver.com>
2022-05-08 23:48:10 +0900

    22/05/08 node.js 공부 자기개발.

---
+++ lecture/url.js
@@ -0,0 +1,8 @@
+/*
+url 모듈 메서드
+기존 노브 방식 메서드
+url.parse : 주소를 분해한다. WHATWG 방식과 비교하면 username 과 password 대신 auth 속성이 있고
+searchParams 대신 query 가 있다.
+
+url.format : WHATWG 방식의 url과 기존 노드의 url 모두 사용가능. 분해되었던 url 객체를 원래 상태로 조립한다.
+ */
