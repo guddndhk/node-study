4e0cb0278fdfbf2399a47660c5bcc21b09178c8c
조형우 <guddndhk@naver.com>
2022-05-11 23:19:10 +0900

    22/05/11 node.js 공부 자기개발. TEST

---
+++ lecture/test.js
@@ -0,0 +1,17 @@
+//
+// function useChk(obj) {
+//     let obj = document.getElementsByName("checkbox1");
+//
+//     for (let i = 0; i < obj.length; i++) {
+//
+//         if (obj[i].checked) {
+//             obj[i].checked = true;
+//         }else if (obj[i].checked === true) {
+//             obj.removeClass("unusedtd"+String(i));
+//         }else if (obj[i].checked === flase) {
+//             obj.addClass("unusedtd"+String(i));
+//         }
+//
+//
+//     }
+// }
