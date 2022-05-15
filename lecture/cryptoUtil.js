ceb81ce4b40c6372c76b5855da2638e59178fe88
조형우 <guddndhk@naver.com>
2022-05-16 03:14:19 +0900

    22/05/16 node.js 공부 자기개발.

--- lecture/cryptoUtil.js
+++ lecture/cryptoUtil.js
@@ -27,3 +27,19 @@ console.log('base64', crypto.createHash('sha512').update('다른 비밀번호').
 node 는 pdkdf2, scrypt 을 지원한다.
 bcrypt 는 지원하지 않는다. 하지만 사용법이 있다.. ㅎ
  */
+
+/*
+pbkdf2 예제
+crypto.randomByte 로 64바이트 문자열 생성 > salt 역할
+pbkdf2 인수로 순서대로 비밀번호, salt , 반복횟수, 출력바이트, 알고리즘
+반복 횟수를 조정해 암호화하는 데 1초 정도 걸리게 맞추는 것이 권장되고 있다.
+salt 랑 비밀번호 둘다 DB 에 저장하는것을 권장 salt 를 쓰는건 해독을 더 어렵게 하기 위해서.
+ */
+
+crypto.randomBytes(64, (err, buf) => {
+    const salt = buf.toString('base64');
+    console.log('salt: ' + salt);
+    crypto.pbkdf2('비밀번호', salt, 100000, 64, 'sha512', (err, key) => {
+        console.log('password: ' + key.toString('base64'));
+    });
+});
